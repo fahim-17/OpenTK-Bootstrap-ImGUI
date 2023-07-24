a928d62414fc84805220ca2e71e99e176dc70ad9
Fahim Ahmed <fahim_ahmed@outlook.com>
2023-07-24 21:07:53 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# OpenTK-Bootstrap-With-ImGUI
+
+## Getting started
+
+- Use this template to create your own repo out of this one
+- Consider renaming the project as its just called `Client`, unless you plan on writing the next 3D-MMO ;)
+
+## Versioning
+
+Open `Version.props` and change `VersionMajor`, `VersionMinor`, `VersionPatch` and `VersionSuffix` to your liking or hook it up to some CI/CD mechanism of your choice.
+
+## Dependencies
+
+`OpenTK` `Serilog` `Microsoft.Extensions.DependencyInjection` `ImGUI.NET`
---
+++ src/Client/ClientGame.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Client.Base;
+using Client.Base.UI;
+using ImGuiNET;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using Serilog;
+
+namespace Client
+{
+    internal sealed class ClientGame : Game
+    {
+        private readonly Color4 _clearColor;
+        private ImGuiController _imGuiController;
+
+        public ClientGame(
+            ILogger logger,
+            IServiceProvider serviceProvider,
+            GameSettings gameSettings)
+            : base(logger, serviceProvider, gameSettings)
+        {
+            Window.Title = "Client Game";
+
+            _clearColor = new Color4(0.1f, 0.1f, 0.1f, 1.0f);
+        }
+
+        protected override void Load()
+        {
+            base.Load();
+            _imGuiController = new ImGuiController(Window.Size.X, Window.Size.Y);
+        }
+
+        protected override void Render(FrameEventArgs e)
+        {
+            GL.Viewport(0, 0, Window.Size.X, Window.Size.Y);
+            GL.ClearColor(_clearColor);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            if (ImGui.Begin("TestWindow"))
+            {
+                if (ImGui.Button("Close"))
+                {
+                    Window.Close();
+                }
+
+                ImGui.End();
+            }
+            _imGuiController.Render();
+            base.Render(e);
+        }
+
+        protected override void Unload()
+        {
+            _imGuiController.Dispose();
+            base.Unload();
+        }
+
+        protected override void Update(FrameEventArgs e)
+        {
+            _imGuiController.Update(Window, (float)e.Time);
+            base.Update(e);
+        }
+    }
+}
